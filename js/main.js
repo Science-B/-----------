a51074a059785efd66ab361309f5cf8e0712d46e
Science-B <dmitriytwd@gmail.com>
2023-07-23 14:42:24 +0300

    added js-scripts

--- js/main.js
+++ js/main.js
@@ -10,3 +10,48 @@ nav.onclick = function () {
   nav.classList.toggle("header__top-row--mobile");
   document.body.classList.toggle("no-scroll");
 };
+
+mask("[data-tel-input]");
+
+/* Phone Mask */
+
+const phoneInputs = document.querySelectorAll("[data-tel-input]");
+phoneInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.value == "+") input.value = "";
+  });
+  input.addEventListener("blur", () => {
+    if (input.value == "+") input.value = "";
+  });
+});
+
+/* Yandex Map */
+
+ymaps.ready(init);
+function init() {
+  var map = new ymaps.Map("map", {
+    center: [59.943543, 30.338928],
+    zoom: 16,
+  });
+
+  var myPlacemark = new ymaps.Placemark(
+    [59.943543, 30.338928],
+    {},
+    {
+      iconLayout: "default#image",
+      iconImageHref: "../img/main/map/location-pin.svg",
+      iconImageSize: [40, 40],
+      iconImageOffset: [-20, -40],
+    }
+  );
+
+  map.controls.remove("geolocationControl");
+  map.controls.remove("searchControl");
+  map.controls.remove("trafficControl");
+  map.controls.remove("typeSelector");
+
+  map.controls.remove("rulerControl");
+  map.behaviors.disable(["scrollZoom"]);
+
+  map.geoObjects.add(myPlacemark);
+}
